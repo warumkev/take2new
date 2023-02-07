781f601ab2284be0a69ae08ca23f41ce85ac2fcb
kev9euf3rois <118884361+kev9euf3rois@users.noreply.github.com>
2023-02-07 20:20:06 +0100

    Optimized sell.php for Elvis

--- sell.php
+++ sell.php
@@ -1,6 +1,84 @@
 <?php
 session_start();
-include('./includes/connect.php');
+
+// Datenbankverbindung aufbauen
+
+$host = "localhost"; //$_ENV['POSTGRES_HOST'];
+$port = "5432";
+$db = "take2new";
+$user = "postgres";
+$pw = ""; //"Start#123";
+$connStr = "host=$host port=$port dbname=$db user=$user password=$pw";
+
+$dbConn = pg_connect($connStr);
+
+if (!$dbConn) {
+    echo "Ein Fehler ist aufgetreten.\n";
+    exit;
+}
+
+// Nutzername abrufen
+
+if (isset($_SESSION['loggedin'])) {
+    $userNameId = $_SESSION['userid'];
+    $getUserName = pg_query($dbConn, "SELECT * FROM public.sellers WHERE id = $userNameId");
+    $currentUserName = pg_fetch_assoc($getUserName);
+    $name = $currentUserName['username'];
+    $userPassword = $currentUserName['userpassword'];
+}
+
+// Artikel hochladen
+
+if (isset($_POST["sell"])) {
+
+    $errors = [];
+    $currentDirectory = getcwd();
+    $uploadDirectory = "/itemimg/";
+    $fileExtensionsAllowed = ['jpeg', 'jpg', 'png'];
+    $imageName = $_FILES['pictureName']['name'];
+    $imageSize = $_FILES['pictureName']['size'];
+    $imageTmpName = $_FILES['pictureName']['tmp_name'];
+    $imageType = $_FILES['pictureName']['type'];
+
+    $tmp = explode('.', $imageName);
+    $imageExtension = strtolower(end($tmp));
+    $uploadPath = $currentDirectory . $uploadDirectory . basename($fileName);
+
+
+
+    if (!in_array($imageExtension, $imageExtensionsAllowed)) {
+        $errors[] = "Ungültiges Dateiformat! Bitte lade eine PNG oder JPG hoch.";
+    }
+
+    if (empty($errors)) {
+        $didUpload = move_uploaded_file($imageTmpName, $uploadPath);
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $size = $_POST['size'];
+        $sellerid = $_SESSION['userid'];
+        $description = nl2br($description);
+        $description = stripslashes($description);
+
+        $articleExists = pg_query($dbConn, "SELECT itemname FROM public.sellers WHERE itemname LIKE '$name'");
+
+        pg_query($dbConn, "INSERT INTO public.items(id, itemname, itemdescription, itemviews, itemprice, sellerid, itemsize, picturename) VALUES (DEFAULT, '$name', '$description', DEFAULT, $price, $sellerid, '$size', '$imageName')");
+
+        header('Location: items.php');
+
+
+        if ($didUpload) {
+            $uploadSuccess = True;
+        } else {
+            echo "Ein Fehler ist aufgetreten. Bitte kontaktiere uns.";
+        }
+    } else {
+        foreach ($errors as $error) {
+            echo $error . " Folgende Fehler traten auf: " . "\n";
+        }
+    }
+
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -22,14 +100,14 @@ include('./includes/connect.php');
         <section class="py-5 text-center container">
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
-                <img src="./assets/brand/take2new-logos_black.png" class="rounded mx-auto d-block" height="60px" width="185" style="margin-top: -80px;"><br>
-                    <h1 class="fw-light" style="margin-top: 70px;">Artikel verkaufen</h1>
+                    <img src="./assets/brand/take2new-logos_black.png" class="rounded mx-auto d-block" height="100px"><br>
+                    <h1 class="fw-light">Artikel verkaufen</h1>
                     <p class="lead text-muted">Wir freuen uns, dass du den Weg zu uns gefunden hast. Hier kannst du die
-                        Informationen zu deinem Piece angeben und die Anzeige online stellen.</p>
+                        Informationen zu dienen Piece angeben und die Anzeige online stellen.</p>
                 </div>
             </div>
         </section>
-        <form method="post"  enctype="multipart/form-data" style="margin-top: -80px;">
+        <form method="post"  enctype="multipart/form-data">
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Artikel</span>
                 <input type="text" class="form-control" placeholder="Artikelname" aria-label="Artikelname"
